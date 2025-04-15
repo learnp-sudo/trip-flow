7a797b9967a4962a61e6eec2d691ea87e3304bd8
Chenna <sreeja.chenna@capgemini.com>
2025-04-15 18:41:32 +0530

    trip-flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+
+export interface Trip {
+  start: string
+  end: string
+  level: number
+  isContinued: boolean
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'path-mapper';
+  title = 'path-mapper'
+  startPoint = ''
+  endPoint = ''
+  trips: Trip[] = []
+
+  addTrip() {
+    if (!this.startPoint || !this.endPoint) return
+    if (!this.trips.length) {
+      let newTrip: Trip = {
+        start: this.startPoint,
+        end: this.endPoint,
+        level: 1,
+        isContinued: false,
+      }
+      this.trips.push(newTrip)
+    }
+    else {
+      const lastTrip: Trip = this.trips[this.trips.length - 1]
+      let newTrip: Trip = {
+        start: this.startPoint,
+        end: this.endPoint,
+        level: 1,
+        isContinued: false,
+      }
+      if (lastTrip && lastTrip.end.toLowerCase() === this.startPoint.toLowerCase()) {
+        newTrip.isContinued = true
+      }
+      else if (lastTrip && lastTrip.end.toLowerCase() === this.endPoint.toLowerCase() && lastTrip.start.toLowerCase() === this.startPoint.toLowerCase()) {
+        newTrip.level = 2
+      }
+      this.trips.push(newTrip)
+    }
+    this.startPoint = ''
+    this.endPoint = ''
+  }
+
+  resetTrip() {
+    this.trips = []
+    this.startPoint = '';
+    this.endPoint = '';
+  }
+
+  getAbbreviation(location: string): string {
+    return location.slice(0, 3).toUpperCase()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
